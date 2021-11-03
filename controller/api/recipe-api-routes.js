d81eed17a202e4980ea5c406b7776f94111e3c80
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-03 18:26:48 -0500

    jquery master race

--- controller/api/recipe-api-routes.js
+++ controller/api/recipe-api-routes.js
@@ -1,27 +1,28 @@
 const { default: axios } = require('axios');
 const router = require('express').Router();
 const User  = require('../../models/user');
-const db = require('../../istest/t')
+const db = require('../../istest/t');
 
-    const labels=db.labels
-     const images=db.images
-     const url1=db.url1
-     const diet2=db.diet2
+    // const labels=db.labels
+    //  const images=db.images
+    //  const url1=db.url1
+    //  const diet2=db.diet2
 
 // &health=
 
 // fix url
-router.get('/:val',async (req,res)=>{
+router.get('/:val', async (req,res)=>{
 
 //console.log(req.body)
-// const url= 'https://api.edamam.com/api/recipes/v2?type=public&q=fries&app_id=7f405668&app_key=eda4d42231735830901807b91c947c66'
+
+// const url == 'https://api.edamam.com/api/recipes/v2?type=public&q=fries&app_id=7f405668&app_key=eda4d42231735830901807b91c947c66'
 
 const queryUrl=`https://api.edamam.com/api/recipes/v2?type=public&q=fries&app_id=7f405668&app_key=eda4d42231735830901807b91c947c66`
 
 let result= await axios.get(queryUrl)
-let result1 = result.data.hits
-
-res.json(result1)
+let recipeDig = result.data.hits
+let recipeDig2=recipeDig[0].recipe
+res.json(recipeDig2)
 
 })
 
--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 // https://api.edamam.com/api/recipes/v2
 const path = require('path');
 const express = require('express')
-//const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controller');
-// const helpers = require('./utils/helpers');
-
-
-// var passport = require("./config/passport");
-
-//const app = express()
-
 const sequelize = require('./config/connection');
-// var passport = require("./config/passport");
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-//const PORT = 3001
+const withAuth = require('./utils/auth');
+
+
+//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const session = require('express-session');
+
 
 const app = express()
-
- const axios = require('axios')
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-
+const axios = require('axios')
 const hbs = exphbs.create({ });
 
 
@@ -36,16 +28,17 @@ const hbs = exphbs.create({ });
 //     db: sequelize
 //   })
 // };
-// app.use(session(sess));
 
-// Inform Express.js on which template engine to use
+// app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
