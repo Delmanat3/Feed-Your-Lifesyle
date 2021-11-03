7b07f27ea4a3fabb9dd0e54190d3eb55cc0ab478
brooke <brookejones406@gmail.com>
2021-11-03 16:45:01 -0500

    fixed homepage and main

--- public/js/main.js
+++ public/js/main.js
@@ -1,92 +1,89 @@
 const { default: axios } = require('axios');
 
+const showMe =  (result)=>{
+
+    console.log(result);
+
+    var row = $("<row>");
+    $("#results").append(row);
+
+     for(var i = 0; i < result.length; i++){
+      //fuckarubaduck
+      var recipeItem = result[i].recipe;
+
+      var favButton = $("<a>")
+      var colDiv = $("<div>");
+      var cardDiv = $("<div>");
+      var cardBlock = $("<div>");
+      var title = $("<h4>");
+      var img = $("<img>");
+      var dietLabels = $("<p>");
+      var fullRecipe = $("<a>");
+
+      //Add attributes to elements
+      favButton.attr({
+
+        name: recipeItem.label,
+
+        class: "",
+
+      });
+
+      colDiv.attr({
+        class: "",
+      });
+
+      cardDiv.attr({
+        class: "",
+      });
+// <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+      img.attr({
+        src: recipeItem.image,
+        alt: recipeItem.label,
+        class: ""
+      })
+
+      cardBlock.attr({
+        class: "",
+        style: ""
+      });
+
+      title.attr({
+        class: "",
+        style: ""
+      })
+
+      title.text(recipeItem.label);
 
 
-// const showMe =  (result)=>{
+      dietLabels.attr({
+        style: ""
+      })
 
-//     console.log(result);
+      console.log(recipeItem.dietLabels);
+      dietLabels.text("Diet Labels : " + recipeItem.dietLabels.toString());
 
-//     var row = $("<row>");
-//     $("#results").append(row);
+      fullRecipe.attr({
+        href: recipeItem.url,
+        class: "",
+        target: "_blank"
+      });
 
-//      for(var i = 0; i < result.length; i++){
-//       //fuckarubaduck
-//       var recipeItem = result[i].recipe;
+      fullRecipe.text("Full Recipe")
 
-//       var favButton = $("<a>")
-//       var colDiv = $("<div>");
-//       var cardDiv = $("<div>");
-//       var cardBlock = $("<div>");
-//       var title = $("<h4>");
-//       var img = $("<img>");
-//       var dietLabels = $("<p>");
-//       var fullRecipe = $("<a>");
+      colDiv.append(cardDiv);
+      cardDiv.append(img);
+      cardDiv.append($("<hr>"))
+      cardBlock.append(title);
+      cardBlock.append($("<hr>"));
+      cardBlock.append(dietLabels);
+      cardBlock.append($("<hr>"));
+      cardBlock.append(fullRecipe);
+      cardDiv.append(cardBlock)
+        $("#results").append(colDiv);
+     }
 
-//       //Add attributes to elements
+  };
 
-//       favButton.attr({
-
-//         name: recipeItem.label,
-
-//         class: "",
-
-//       });
-
-//       colDiv.attr({
-//         class: "",
-//       });
-
-//       cardDiv.attr({
-//         class: "",
-//       });
-// // <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-//       img.attr({
-//         src: recipeItem.image,
-//         alt: recipeItem.label,
-//         class: ""
-//       })
-
-//       cardBlock.attr({
-//         class: "",
-//         style: ""
-//       });
-
-//       title.attr({
-//         class: "",
-//         style: ""
-//       })
-
-//       title.text(recipeItem.label);
-
-
-//       dietLabels.attr({
-//         style: ""
-//       })
-
-//       console.log(recipeItem.dietLabels);
-//       dietLabels.text("Diet Labels : " + recipeItem.dietLabels.toString());
-
-//       fullRecipe.attr({
-//         href: recipeItem.url,
-//         class: "",
-//         target: "_blank"
-//       });
-
-//       fullRecipe.text("Full Recipe")
-
-//       colDiv.append(cardDiv);
-//       cardDiv.append(img);
-//       cardDiv.append($("<hr>"))
-//       cardBlock.append(title);
-//       cardBlock.append($("<hr>"));
-//       cardBlock.append(dietLabels);
-//       cardBlock.append($("<hr>"));
-//       cardBlock.append(fullRecipe);
-//       cardDiv.append(cardBlock)
-//         $("#results").append(colDiv);
-//      }
-
-//   };
-
-// loader function
+loader function
 
